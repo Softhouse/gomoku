c06adcffc5a700901a7f37f72b9ce3bf933f42a7
Johan Kardell <johan.kardell@gmail.com>
2014-08-05 09:20:06 +0200

    Removed all express modules (no longer supported or used)

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
